b7e62fe3de4038e56489f97f289b9566c7f9ebe6
hokey_imac <substantiation@naver.com>
2020-07-21 02:37:48 +0900

    express - send html/css

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // console.log(__dirname)
+ 
+  fs.readFile('./public/main/login.html','UTF-8',(err,data)=>{
+    if(err){
+      next(err);
+      return;
+    }
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end(data);
+  })
 });
 
 module.exports = router;
