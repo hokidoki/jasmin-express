e4bc750161cf7ae761ed78789c18c9942940583b
hokey_imac <substantiation@naver.com>
2020-07-21 00:04:00 +0900

    only-http middleware critical section problom fix

--- only_http/middlewares.js
+++ only_http/middlewares.js
@@ -5,26 +5,28 @@ const Middleware = () => {
         _middlewares.push(fn)
     }
 
-    const _run = (i, err) => {
-
-        if (i < 0 || i >= _middlewares.length) return;
-
-        const nextMw = _middlewares[i]
-        const next = err => _run(i + 1, err)
-
-        nextMw(_req, _res, next);
-
-        if (err) {
-            const isNextErrorMw = nextMw.length === 4
-
-            return isNextErrorMw ?
-                nextMw(err, _req, _res, next) :
-                _run(i + 1, err)
-        }
-    }
+    
     const run = (req, res) => {
-        _req = req;
-        _res = res;
+        const _req = req;
+        const _res = res;
+
+        const _run = (i, err) => {
+
+            if (i < 0 || i >= _middlewares.length) return;
+    
+            const nextMw = _middlewares[i]
+            const next = err => _run(i + 1, err)
+    
+            nextMw(_req, _res, next);
+    
+            if (err) {
+                const isNextErrorMw = nextMw.length === 4
+    
+                return isNextErrorMw ?
+                    nextMw(err, _req, _res, next) :
+                    _run(i + 1, err)
+            }
+        }
 
         _run(0);
     }
