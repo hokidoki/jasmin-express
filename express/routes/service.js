09f9f91e5cb5baf19654f9828a3dc3946a981638
hokey_imac <substantiation@naver.com>
2020-07-23 02:08:13 +0900

    express - insert user info

--- express/routes/service.js
+++ express/routes/service.js
@@ -5,28 +5,31 @@ const mysql = require('../db/mysql').init();
 
 
 /* GET home page. */
-router.get('/join', function(req, res, next) {
-  fs.readFile('./public/main/join.html','UTF-8',(err,data)=>{
-    if(err){
+router.get('/join', function (req, res, next) {
+  fs.readFile('./public/main/join.html', 'UTF-8', (err, data) => {
+    if (err) {
       next(err);
       return;
     }
-    res.writeHead(200,{'Content-Type':'text/html'});
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(data);
   })
 });
 
-router.post('/join', function(req, res, next) {
-    console.log(req.body);
-    try{
-      mysql.query(`insert into account(id,pw,email) values('${req.body.id}','${req.body.password_1}','${req.body.email}')`,(result,err)=>{
-        console.log(result,err);
-      });
-    }catch(e){
-      console.log(e);
+router.post('/join', function (req, res, next) {
+  mysql.query(`call jasmin_express.join('${req.body.id}','${req.body.password_1}','${req.body.email}')`, (err, result) => {
+    if (err) {
+      console.log(err);
+      next(err);
+      return;
     }
-    
-    res.send("OK")
+    console.log(result);
+    if(result[0][0].status){
+      res.send("id already used");
+    }else{
+      res.send("OK");
+    }
   });
+});
 
 module.exports = router;
