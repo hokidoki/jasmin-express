9cc9793af7129f16e6776274c9a21176b58d7ae2
이종호 <substantiation@naver.com>
2020-07-27 15:49:15 +0900

    created main.pug

--- express/app.js
+++ express/app.js
@@ -11,12 +11,12 @@ const mysqlConnect = require('./db/mysql');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var serviceRouter = require('./routes/service')
-
+var mainRouter = require('./routes/main');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/service',serviceRouter);
 app.use('/users', usersRouter);
+app.use('/main',mainRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
