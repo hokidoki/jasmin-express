3258e10249827b2214bd889f034e5cc5c106862f
hokey_imac <substantiation@naver.com>
2020-08-05 00:56:07 +0900

    express - get Article

--- express/routes/main.js
+++ express/routes/main.js
@@ -1,9 +1,19 @@
 var express = require('express');
+const { json } = require('express');
+const mysql = require('../db/mysql').init();
 var router = express.Router();
 var isValidAccess = require('./middleware/cookie').default;
 /* GET home page. */
-router.get('/', isValidAccess,function(req, res, next) {
-    res.render('main')
+router.get('/', isValidAccess, function (req, res, next) {
+    mysql.query('call get_article()', (err, result) => {
+        if(err){
+            next(new Error("문제가 발생하였습니다."));
+            return;
+        }
+        res.render('main', {
+            articleList : result[0]
+        })
+    })
 });
 
 module.exports = router;
